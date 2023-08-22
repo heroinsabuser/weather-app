daf596c63117289ed35a2e4eb3fdb92e52f0a7fc
heroinsabuser <maksim-kozlov-2002@mail.ru>
2023-08-22 18:51:13 +0300

    fix some bugs

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const container = document.querySelector('.container')
-const search = document.querySelector('.search-box button')
-const weatherBox = document.querySelector('.weather-box')
-const weatherDetails = document.querySelector('.weather-details')
-const error404 = document.querySelector('.not-found')
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () =>{
 
     const APIKey = '4d56b4db93c91aba83877ae240fba44f';
     const city = document.querySelector('.search-box input').value;
 
-    if(city=='')
+    if(city==='')
         return;
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
@@ -30,7 +30,7 @@ search.addEventListener('click', () =>{
 
             const image = document.querySelector('.weather-box img');
             const temperature = document.querySelector('.weather-box .temperature');
-            const description = document.querySelector('weather-box .description');
+            const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
 
@@ -59,7 +59,7 @@ search.addEventListener('click', () =>{
                         image.src = '';
             }
 
-            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
             description.innerHTML = `${json.weather[0].description}`;
             humidity.innerHTML = `${json.main.humidity}%`;
             wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
